9a0dfd3e9fe7379a56f81945e0eec5b7388b121a
80546 <143219013+Wilson546@users.noreply.github.com>
2023-12-11 17:37:04 +0000

    first commit

---
+++ auto-analysis.py
@@ -0,0 +1,6 @@
+import pandas as pd
+from matplotlib import pyplot as plt
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+
